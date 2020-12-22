d6f2fe7be64754d265c91d30a63abf23423cd3b8
Samuel Wang <swang2023@gmail.com>
2020-12-22 14:47:06 -0500

    Added Search Component
    Adde the search componenet to the app

---
+++ src/components/Search.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="search-wrapper">
+      <input type="text" v-model="search" :placeholder=placeholder />
+  </div>
+  <div class="search-results">
+      
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Search',
+ 
+  components: {
+    
+  },
+
+   props: {
+    placeholder: String,
+  }
+}
+</script>
+    
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
+</style>
