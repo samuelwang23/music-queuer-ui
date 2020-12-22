287442bbefcdea7b4c6a2cb3d9e1bd858bd9cf3c
Samuel Wang <swang2023@gmail.com>
2020-12-22 18:22:40 -0500

    Search Result Component
    Added the search result component

---
+++ src/components/SearchResult.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="card">
+    <div>
+      <figure>
+        <img :src=album.art :alt=album.title >       
+     </figure>
+   </div>
+    <div>
+      <div>
+        <div>
+          <div><a>{{album.title}}</a></div>
+          <div>
+            {{album.artist}}<br>
+            <em>Released {{album.release}}</em>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchResult',
+  props: {
+    album: Object,
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
